a96b22cffe62367e327c33fb56d471486bd2ee3e
JarodG64 <julien.rigolini@pm.me>
2024-12-27 09:08:18 +0100

    [sodium] Ajustements dans le code de install.sh

--- install.sh
+++ install.sh
@@ -5,24 +5,22 @@
 
 # Installe le script et configure les permissions
 echo "Installation du script :"
-sudo cp -uv new_year_dirs.sh /usr/local/bin/new_year_dirs.sh
+sudo cp -v new_year_dirs.sh /usr/local/bin/new_year_dirs.sh
 sudo chmod 755 /usr/local/bin/new_year_dirs.sh
 
 # Installe les services et configure les permissions
 echo " "
 echo "Installation du service et du timer :"
-sudo cp -uv new_year_dirs.service /etc/systemd/system/new_year_dirs.service
+sudo cp -v new_year_dirs.service /etc/systemd/system/new_year_dirs.service
 sudo chmod 644 /etc/systemd/system/new_year_dirs.service
-sudo cp -uv new_year_dirs.timer /etc/systemd/system/new_year_dirs.timer
+sudo cp -v new_year_dirs.timer /etc/systemd/system/new_year_dirs.timer
 sudo chmod 644 /etc/systemd/system/new_year_dirs.timer
 sudo systemctl daemon-reload
 echo " "
 echo "Configuration du service et du timer :"
 sudo systemctl enable new_year_dirs.service
 sudo systemctl enable new_year_dirs.timer
-#sudo systemctl stop new_year_dirs.service
 sudo systemctl start new_year_dirs.timer
-echo " "
 sudo systemctl status new_year_dirs.service
 echo " "
 sudo systemctl status new_year_dirs.timer
